a5b17e60f419a31839f72a00ca6bb4a7ecf2fc2a
Kumar Harsh <125875956+harshu-kasyap@users.noreply.github.com>
2023-09-02 18:56:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,154 +1,88 @@
 import cv2
+import firebase_admin
+from firebase_admin import credentials, storage
+import streamlit as st
+
+from DataToStore import min_area
+
+# Initialize the Firebase Admin SDK
+cred = credentials.Certificate("service_account.json")
+
+# Check if Firebase Admin SDK is already initialized
+if not firebase_admin._apps:
+    firebase_admin.initialize_app(cred, {
+        'storageBucket': 'hackarthon-b3435.appspot.com'
+    })
 
 # License Plate Detection
-
 harcascade = "model/haarcascade_russian_plate_number.xml"
-import cv2
 
-# License Plate Detection
+# Create a Streamlit title and description
+st.title("DVR Footage Analyser")
+# st.write("This Streamlit app detects license plates using OpenCV and uploads them to Firebase Cloud Storage.")
 
-harcascade = "model/haarcascade_russian_plate_number.xml"
-video_path = r"C:\Users\harsh\OneDrive\Desktop\veh2.mp4"
-cap = cv2.VideoCapture(1)
+# Create a Streamlit placeholder for displaying images
+image_placeholder = st.empty()
 
-cap.set(3, 640)  # Width
-cap.set(4, 480)  # Height
+# Create a button to start/stop the detection process
+start_detection = st.button("Start Detection")
+stop_detection = st.button("Stop Detection")
 
-min_area = 500
+cap = None
+detection_running = False
 count = 0
 
-# Vehicle Speed Detection
-
-previous_frame = None
-fps = cap.get(cv2.CAP_PROP_FPS)
-scale_factor = 1
+# Firebase Cloud Storage bucket
+bucket = storage.bucket()
 
 while True:
-    success, img = cap.read()
+    if start_detection and not detection_running:
+        cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)  # Use DirectShow on Windows
+  # Use 0 for the default camera
+        cap.set(3, 640)  # Width
+        cap.set(4, 480)  # Height
+        detection_running = True
+        start_detection = False
 
-    if not success:
-        break
+    if stop_detection and detection_running:
+        cap.release()
+        cv2.destroyAllWindows()
+        detection_running = False
+        stop_detection = False
 
-    # License Plate Detection
-    plate_cascade = cv2.CascadeClassifier(harcascade)
-    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    plates = plate_cascade.detectMultiScale(img_gray, 1.1, 4)
+    if detection_running:
+        success, img = cap.read()
 
-    for (x, y, w, h) in plates:
-        area = w * h
+        if not success:
+            break
 
-        if area > min_area:
-            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            cv2.putText(img, "Number Plate", (x, y - 5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255, 0, 255), 2)
+        # License Plate Detection
+        plate_cascade = cv2.CascadeClassifier(harcascade)
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        plates = plate_cascade.detectMultiScale(img_gray, 1.1, 4)
 
-            img_roi = img[y: y + h, x:x + w]
+        for (x, y, w, h) in plates:
+            area = w * h
 
-            # Save the captured plate image
-            if 'img_roi' in locals():
+            if area >min_area:
+                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                cv2.putText(img, "Number Plate", (x, y - 5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255, 0, 255), 2)
+
+                img_roi = img[y: y + h, x:x + w]
+
+                # Save the captured plate image to Firebase Cloud Storage
                 plate_filename = "plates/scaned_plate_" + str(count) + ".jpg"
-                cv2.imwrite(plate_filename, img_roi)
-                print(f"Plate image saved as {plate_filename}")
+                _, img_encoded = cv2.imencode(".jpg", img_roi)
+                img_bytes = img_encoded.tobytes()
+                blob = bucket.blob("plate/" + plate_filename)
+                blob.upload_from_string(img_bytes, content_type="image/jpeg")
+                st.write(f"Plate image uploaded to Firebase Cloud Storage as {blob.name}")
                 count += 1
 
-    # Vehicle Speed Detection
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # Display the image with detected plates in Streamlit
+        image_placeholder.image(img, channels="BGR", use_column_width=True)
 
-    if previous_frame is None:
-        previous_frame = gray
-        continue
-
-    frame_diff = cv2.absdiff(previous_frame, gray)
-    _, thresh = cv2.threshold(frame_diff, 30, 255, cv2.THRESH_BINARY)
-    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    for contour in contours:
-        if cv2.contourArea(contour) < 100:
-            continue
-
-        x, y, w, h = cv2.boundingRect(contour)
-        speed = (w / scale_factor) * fps * 3.6
-
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        cv2.putText(img, f"Speed: {speed:.2f} km/h", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-
-    cv2.imshow("License Plate and Speed Detection", img)
-
-    previous_frame = gray
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
-
-cap = cv2.VideoCapture(video_path)
-
-cap.set(3, 640)  # Width
-cap.set(4, 480)  # Height
-
-min_area = 500
-count = 0
-
-# Vehicle Speed Detection
-
-previous_frame = None
-fps = cap.get(cv2.CAP_PROP_FPS)
-scale_factor = 1
-
-while True:
-    success, img = cap.read()
-
-    if not success:
-        break
-
-    # License Plate Detection
-    plate_cascade = cv2.CascadeClassifier(harcascade)
-    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    plates = plate_cascade.detectMultiScale(img_gray, 1.1, 4)
-
-    for (x, y, w, h) in plates:
-        area = w * h
-
-        if area > min_area:
-            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            cv2.putText(img, "Number Plate", (x, y - 5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255, 0, 255), 2)
-
-            img_roi = img[y: y + h, x:x + w]
-
-            # Save the captured plate image
-            if 'img_roi' in locals():
-                plate_filename = "plates/scaned_plate_" + str(count) + ".jpg"
-                cv2.imwrite(plate_filename, img_roi)
-                print(f"Plate image saved as {plate_filename}")
-                count += 1
-
-    # Vehicle Speed Detection
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    if previous_frame is None:
-        previous_frame = gray
-        continue
-
-    frame_diff = cv2.absdiff(previous_frame, gray)
-    _, thresh = cv2.threshold(frame_diff, 30, 255, cv2.THRESH_BINARY)
-    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    for contour in contours:
-        if cv2.contourArea(contour) < 100:
-            continue
-
-        x, y, w, h = cv2.boundingRect(contour)
-        speed = (w / scale_factor) * fps * 3.6
-
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        cv2.putText(img, f"Speed: {speed:.2f} km/h", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-
-    cv2.imshow("License Plate and Speed Detection", img)
-
-    previous_frame = gray
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cap.release()
+# Release resources when the app is closed
+if cap is not None:
+    cap.release()
 cv2.destroyAllWindows()
